cc9c136be8d33088ca71f6cbace0ee22de03c9c8
AJ <1018212@student.sae.edu.au>
2020-02-27 10:40:45 +1100

    Added changed stuff, movement scripts and mateerials

---
+++ Assets/throwDummy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwDummy : MonoBehaviour
+{
+    [Header("The Pacifier Speed")]
+    public float dummyForce = 40f;
+    public GameObject dummyPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DummyThrow();
+        }
+    }
+    void DummyThrow()
+    {
+        GameObject dummy = Instantiate(dummyPrefab, transform.position, transform.rotation);
+        Rigidbody rb = dummy.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * dummyForce, ForceMode.VelocityChange);
+    }
+}
