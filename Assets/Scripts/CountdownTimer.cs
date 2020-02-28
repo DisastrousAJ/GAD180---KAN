9ec704c8ac5087aac650c26d9e1309e35cb29f58
Karan <karan_gtr@live.com.au>
2020-02-29 00:24:48 +1100

    Timer (Not done yet)

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+
+	float currentTime = 0f;
+	float startingTime = 120f;
+
+	[SerializeField] Text countdownText;
+
+	private void Start()
+	{
+		currentTime = startingTime;
+	}
+
+	private void Update()
+	{
+		currentTime -= 1 * Time.deltaTime;
+		countdownText.text = currentTime.ToString("0");
+
+
+
+		if (currentTime <= 0)
+		{
+			currentTime = 0;
+		}
+
+	}
+
+}
