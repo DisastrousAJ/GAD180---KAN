6d213b531eaafe9c4f4fcec8976a907d6dba96da
AJ <1018212@student.sae.edu.au>
2020-03-04 10:17:03 +1100

    Babies move away from objects

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public GameObject chest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chest = GameObject.Find("Chest");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToChest = Vector3.Distance(this.transform.position, chest.transform.position);
+
+
+
+        if (distanceToChest < 5)
+        {
+            AvoidChest();
+        }
+
+        else
+        {
+            Idle();
+        }
+    }
+
+
+    void Idle()
+    {
+
+    }
+
+    void AvoidChest()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, chest.transform.position, -0.02f);
+    }
+}
---
+++ Assets/Scripts/Cot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cot : MonoBehaviour
+{
+    public GameObject cot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cot = GameObject.Find("Cot");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToCot = Vector3.Distance(this.transform.position, cot.transform.position);
+
+
+
+        if (distanceToCot < 5)
+        {
+            AvoidCot();
+        }
+
+        else
+        {
+            Idle();
+        }
+    }
+
+
+    void Idle()
+    {
+
+    }
+
+    void AvoidCot()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, cot.transform.position, -0.02f);
+    }
+}
