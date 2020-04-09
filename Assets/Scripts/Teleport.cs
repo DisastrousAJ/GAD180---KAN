852d0e3be74d7bff69f6cda027755237b442b929
AJ <1018212@student.sae.edu.au>
2020-04-09 13:44:17 +1000

    Teleportation for babies is complete

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+ 
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.position = new Vector3(Random.Range(11.0f, -13.0f), col.transform.position.y, Random.Range(18.0f, -15.0f));
+    }
+}
