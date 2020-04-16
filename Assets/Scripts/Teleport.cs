1beaf7e37d220995535e7550d2854dc703506d2e
AJ <1018212@student.sae.edu.au>
2020-04-16 10:52:19 +1000

    Added teleports and particles to scene

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+       // if ()
+       // {
+            col.transform.position = new Vector3(Random.Range(-13.0f, 15.0f), col.transform.position.y, Random.Range(-14.0f, 12.0f));
+       // }
+       // else if ()
+       // {
+
+        //}
+    }    
+}
